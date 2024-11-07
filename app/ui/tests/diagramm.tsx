5af48f3d1beeaac753bec6dc016c4d8d332cefe0
Iana <iankuzn21@gmail.com>
2024-11-07 15:54:10 +0500

    Добавление диаграммы

--- app/ui/tests/diagramm.tsx
+++ app/ui/tests/diagramm.tsx
@@ -1,10 +1,36 @@
 'use client';
+import React, { PureComponent } from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Bar, BarChart } from 'recharts';
 
-export default function Diagramm({data}: any) {
+const data = [
+  { name: "Yes", value: 35 },
+  { name: "Not sure", value: 21 },
+  { name: "No", value: 13 },
+];
 
-  return (
-    <>
-      
-    </>
-  );
+export default class Example extends PureComponent {
+
+  render() {
+    return (
+      <ResponsiveContainer
+      width={"100%"}
+      height={"100%"}
+    >
+      <BarChart
+        layout="vertical"
+        data={data}
+        margin={{
+          top: 20,
+          right: 20,
+          bottom: 20,
+          left: 20,
+        }}
+      >
+        <XAxis type="number" />
+        <YAxis dataKey="name" type="category" />
+        <Bar dataKey="value" fill="#413ea0" />
+      </BarChart>
+    </ResponsiveContainer>
+    );
+  }
 }
--- app/ui/tests/scoreCard.tsx
+++ app/ui/tests/scoreCard.tsx
@@ -7,7 +7,7 @@ export default function ScoreCard({ quizResult, testId }: any) {
     <h1 className={`mb-6 text-xl md:text-2xl font-bold`}>Результат теста</h1>
       {testId === 'mbi' ? (
         <div>
-          <Diagramm data={quizResult} />
+          {/* <Diagramm data={quizResult} /> */}
           {quizResult.param_1}
           {quizResult.param_2}
           {quizResult.param_3}
